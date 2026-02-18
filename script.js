d4c9f7b279b4e5d08c503ba1b163f605b1fa1c88
Tavuq <cemojryt@gmail.com>
2026-02-18 18:38:18 +0300

    script.js

--- script.js
+++ script.js
@@ -1,52 +1,68 @@
-        let ua = navigator.userAgent;
-        let np = navigator.platform;
+let ua = navigator.userAgent;
+let np = navigator.platform;
 
-        let osName = "Unknown";
+let osName = "Unknown";
 
-        if (ua.includes("Android")) {
-            osName = "Android";
-        }
+if (ua.includes("Android")) {
+    osName = "Android";
+}
 
-        else if (ua.includes("iPhone") || (ua.includes("iPad"))) {
-            osName = "iOS";
-        }
+else if (ua.includes("iPhone") || (ua.includes("iPad"))) {
+    osName = "iOS";
+}
 
-        else if (np.includes("Win")) {
-            osName = "Windows";
-        }
+else if (np.includes("Win")) {
+    osName = "Windows";
+}
 
-        else if (np.includes("Mac")) {
-            osName = "macOS";
-        }
+else if (np.includes("Mac")) {
+    osName = "macOS";
+}
 
-        else if (np.includes("Linux")) {
-            osName = "Linux";
-        }
+else if (np.includes("Linux")) {
+    osName = "Linux";
+}
 
-        document.getElementById("os").textContent = osName;
+document.getElementById("os").textContent = osName;
 
-        let browserName = "Unknown";
+let browserName = "Unknown";
 
-        if (ua.includes("Edg")) {
-            browserName = "Edge";
-        }
+if (ua.includes("Edg")) {
+    browserName = "Edge";
+}
 
-        else if (ua.includes("Firefox")) {
-            browserName = "Firefox";
-        }
+else if (ua.includes("Firefox")) {
+    browserName = "Firefox";
+}
 
-        else if (ua.includes("Chrome")) {
-            browserName = "Chrome";
-        }
+else if (ua.includes("Chrome")) {
+    browserName = "Chrome";
+}
 
-        else if (ua.includes("Safari")) {
-            browserName = "Safari";
-        }
+else if (ua.includes("Safari")) {
+    browserName = "Safari";
+}
 
-        document.getElementById("browser").textContent = browserName;
+document.getElementById("browser").textContent = browserName;
 
-        document.getElementById("screenResolution").textContent = screen.width + " x " + screen.height;
+document.getElementById("screenResolution").textContent = screen.width + " x " + screen.height;
 
-        fetch("https://api.ipify.org?format=json").then(Response => Response.json()).then(data => {
-            document.getElementById("IP").textContent = data.ip;
-        });
+fetch("https://api.ipify.org?format=json").then(Response => Response.json()).then(data => {
+    document.getElementById("IP").textContent = data.ip;
+});
+
+loader.style.opacity = "0";
+
+window.addEventListener("load", () => {
+
+    setTimeout(() => {
+        document.getElementById("loader").style.display = "none";
+
+        document.getElementById("content").style.display = "block";
+
+        document.getElementById("content").classList.add("animate");
+
+        document.getElementById("content").classList.add("show");
+    }, 2000);
+
+});
